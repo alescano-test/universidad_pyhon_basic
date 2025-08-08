7f70741254f2f9b88ee617270786fb8d3f193892
Alejandro Lescano <alescano.test@gmail.com>
2025-08-08 01:00:45 -0300

    Ejercicio valor_positivo.py finalizado

--- valor_dentro_rango.py
+++
@@ -1,8 +0,0 @@
-VALOR_MINIMO = 0
-VALOR_MAXIMO = 10
-
-valor_ingresado = int(input(f"Ingresa un número dentro del rango {VALOR_MINIMO} y {VALOR_MAXIMO}: "))
-
-es_valido = (valor_ingresado >= VALOR_MINIMO and valor_ingresado <= VALOR_MAXIMO)
-
-print(f"El valor ingresado esta dentro del rango?: {es_valido}")
