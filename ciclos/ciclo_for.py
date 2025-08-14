58be4202bba102dbbf0d04e0084f1a931fef395b
Miguel Alejandro Lescano <alescano.test@gmail.com>
2025-08-14 20:09:41 -0300

    Ciclo for

---
+++ ciclos/ciclo_for.py
@@ -0,0 +1,14 @@
+print('\nCiclo "For"')
+
+print('\nRecorremos una palabra\n')
+secuencia = 'Alejandro'
+
+for letra in secuencia:
+    print(letra, end=' ')
+
+
+print('\n\nRecorremos una lista\n')
+lista_frutas = ['Banana', 'Manzana', 'Mango']
+
+for fruta in lista_frutas:
+    print(fruta, end=' ')
